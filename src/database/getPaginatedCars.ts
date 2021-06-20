154bf9254fca793403f9250465fc03e286ff051f
badster-git <badster@airmail.cc>
2021-06-20 12:27:25 -0700

    added non-null assertions

--- src/database/getPaginatedCars.ts
+++ src/database/getPaginatedCars.ts
@@ -15,15 +15,15 @@ const mainQuery = `
 export async function getPaginatedCars(query: ParsedUrlQuery) {
   const db = await openDB();
 
-  const page = getValueNumber(query.page) || 1;
-  const rowsPerPage = getValueNumber(query.rowsPerPage) || 4;
+  const page = getValueNumber(query.page!) || 1;
+  const rowsPerPage = getValueNumber(query.rowsPerPage!) || 4;
   const offset = (page - 1) * rowsPerPage;
 
   const dbParams = {
-    "@make": getValueStr(query.make),
-    "@model": getValueStr(query.model),
-    "@minPrice": getValueNumber(query.minPrice),
-    "@maxPrice": getValueNumber(query.maxPrice),
+    "@make": getValueStr(query.make!),
+    "@model": getValueStr(query.model!),
+    "@minPrice": getValueNumber(query.minPrice!),
+    "@maxPrice": getValueNumber(query.maxPrice!),
   };
 
   const carsPromise = db.all<CarModel[]>(
@@ -42,12 +42,12 @@ export async function getPaginatedCars(query: ParsedUrlQuery) {
 
   const [cars, totalRows] = await Promise.all([carsPromise, totalRowsPromise]);
 
-  return { cars, totalPages: Math.ceil(totalRows.count / rowsPerPage) };
+  return { cars, totalPages: Math.ceil(totalRows!.count / rowsPerPage) };
 }
 
 function getValueNumber(value: string | string[]) {
   const str = getValueStr(value);
-  const number = parseInt(str);
+  const number = parseInt(str!);
   return isNaN(number) ? null : number;
 }
 
