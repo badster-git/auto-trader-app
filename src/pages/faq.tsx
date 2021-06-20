87113d36085a7ee72f628d269275aa0710128d23
badster-git <badster@airmail.cc>
2021-06-20 12:36:59 -0700

    fixed sqlite bug

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -36,6 +36,6 @@ export default function Faq({ faq }: FaqProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const db = await openDB();
-  const faq = await db.all("SELECT * FROM FAQ ORDER BY createDate DESC");
+  const faq = await db.all("SELECT * FROM faq ORDER BY createDate DESC");
   return { props: { faq } };
 };
